9ca8cea9fcf36a6e951f3adf2f5b09d8190a94d3
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-07 16:17:45 -0300

    Exceptions, validation and security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslearnbds.services;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = repository.findByEmail(s);
+        if (user == null) {
+            logger.error("User not found: " + s);
+            throw new UsernameNotFoundException("User not found");
+        }
+        logger.info("User found: " + s);
+
+        return user;
+    }
+}
+
