7c88045c829f762685f5c1a6a7f3b99f58e0db1d
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-26 20:09:43 -0300

    new research notification

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +22,10 @@ public class NotificationResource {
     private NotificationService service;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(Pageable pageable) {
-        Page<NotificationDTO> page = service.notificationsForCurrentUser(pageable);
+    public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(
+            @RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable) {
+        Page<NotificationDTO> page = service.notificationsForCurrentUser(unreadOnly, pageable);
         return ResponseEntity.ok().body(page);
     }
 }
